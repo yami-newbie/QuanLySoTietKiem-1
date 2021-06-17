54845e7aa0ca791b46fc4f4f1e848d009f1862c0
Hào Đinh Việt <vhao1509@gmail.com>
2021-06-18 01:37:11 +0700

    Nong cung Euro2021, Add Withdraw and ReportView

--- QuanLySoTietKiem/QuanLySoTietKiem/ViewModel/ReportViewModel.cs
+++ QuanLySoTietKiem/QuanLySoTietKiem/ViewModel/ReportViewModel.cs
@@ -1,16 +1,251 @@
-﻿using System;
+﻿using QuanLySoTietKiem.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace QuanLySoTietKiem.ViewModel
 {
     public class ReportViewModel : BaseViewModel
     {
+        private ObservableCollection<ThongKe1> _List1;
+        public ObservableCollection<ThongKe1> List1 { get => _List1; set { _List1 = value; OnPropertyChanged(); } }
+        private ObservableCollection<ThongKe2> _List2;
+        public ObservableCollection<ThongKe2> List2 { get => _List2; set { _List2 = value; OnPropertyChanged(); } }
+        private ObservableCollection<LOAITIETKIEM> _ListLoaiTietKiem;
+        public ObservableCollection<LOAITIETKIEM> ListLoaiTietKiem { get => _ListLoaiTietKiem; set { _ListLoaiTietKiem = value; OnPropertyChanged(); } }
+        private ObservableCollection<SOTIETKIEM> _ListSTK;
+        public ObservableCollection<SOTIETKIEM> ListSTK { get => _ListSTK; set { _ListSTK = value; OnPropertyChanged(); } }
+        private ObservableCollection<PHIEUGOITIEN> _ListGoiTien;
+        public ObservableCollection<PHIEUGOITIEN> ListGoiTien { get => _ListGoiTien; set { _ListGoiTien = value; OnPropertyChanged(); } }
+        private ObservableCollection<PHIEURUTTIEN> _ListRutTien;
+        public ObservableCollection<PHIEURUTTIEN> ListRutTien { get => _ListRutTien; set { _ListRutTien = value; OnPropertyChanged(); } }
+
+        private LOAITIETKIEM _SelectedLoai;
+        public LOAITIETKIEM SelectedLoai { get => _SelectedLoai; set { _SelectedLoai = value; CheckLTKvsThang(); OnPropertyChanged(); } }
+
+        private ThongKe2 _SelectedItem;
+        public ThongKe2 SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+
+                OnPropertyChanged();
+                if(SelectedItem!=null)
+                MessageBox.Show(SelectedItem.SoMo.ToString());                
+
+
+
+            }
+        }    
+        private int _SelectedThang;
+        public int SelectedThang { get => _SelectedThang; set { _SelectedThang = value; CheckLTKvsThang(); OnPropertyChanged(); } }
+        private int[] _cbxThang = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        public int [] cbxThang { get => _cbxThang; set { _cbxThang = value;  OnPropertyChanged(); } }
+        private int _STT;
+        public int STT { get => _STT; set { _STT = value; OnPropertyChanged(); } }
+        private string _LoaiTietKiem;
+        public string LoaiTietKiem { get => _LoaiTietKiem; set { _LoaiTietKiem = value; OnPropertyChanged(); } }
+        private DateTime? _DateInput;
+        public DateTime? DateInput { get => _DateInput; set { _DateInput = value; CheckDateIn(); OnPropertyChanged(); } }
+        private int _TongThu;
+        public int TongThu { get => _TongThu; set { _TongThu = value; OnPropertyChanged(); } }
+        private int _TongChi;
+        public int TongChi { get => _TongChi; set { _TongChi = value; OnPropertyChanged(); } }
+
+        private int _ChenhLech;
+        public int ChenhLech { get => _ChenhLech; set { _ChenhLech = value; OnPropertyChanged(); } }
+      
         public ReportViewModel()
         {
+            
+            List1 = new ObservableCollection<ThongKe1>();
+            List2 = new ObservableCollection<ThongKe2>();
+            ListLoaiTietKiem = new ObservableCollection<LOAITIETKIEM>(DataProvider.Ins.DB.LOAITIETKIEMs);
+            ListSTK = new ObservableCollection<SOTIETKIEM>(DataProvider.Ins.DB.SOTIETKIEMs);
+            ListGoiTien = new ObservableCollection<PHIEUGOITIEN>(DataProvider.Ins.DB.PHIEUGOITIENs);
+            ListRutTien = new ObservableCollection<PHIEURUTTIEN>(DataProvider.Ins.DB.PHIEURUTTIENs);
+        }
+        public void CheckDateIn()
+        {
+            List1.Clear();
+            int i = 0;
+            if(ListLoaiTietKiem.Count()>0)
+            foreach(var _ltk in ListLoaiTietKiem)
+            {
+                i++;
+                List1.Add(new ThongKe1 { STT = i, LoaiTietKiem = _ltk.TenLoaiTietKiem, TongThu = (int)TongTienGoi1Ngay(_ltk.TenLoaiTietKiem),TongChi = (int)TongTienRut1Ngay(_ltk.TenLoaiTietKiem),ChenhLech= (int)TongTienGoi1Ngay(_ltk.TenLoaiTietKiem) - (int)TongTienRut1Ngay(_ltk.TenLoaiTietKiem) });
+            }
+        }
+        public void CheckLTKvsThang()
+        {
+            List2.Clear();
+            if(SelectedLoai!=null && (SelectedThang>=1 &&SelectedThang<=12))
+            {
+                TongSo(SelectedLoai.TenLoaiTietKiem, SelectedThang);
+            }
+        }
+
+
+        public double TongTienGoi1Ngay(string strLoaiTietKiem)
+        {
+            int sumGoi = 0;
+            var inputSTK = ListSTK.Where(x => x.LOAITIETKIEM.TenLoaiTietKiem == strLoaiTietKiem);
+            var formatDateInput = String.Format("{0:d}", DateInput);
+            foreach (var item in inputSTK)
+            {
+                var phieuGoiTien = ListGoiTien.Where(x => x.MaSo == item.MaSo);
+                foreach (var itemPhieuGoiTien in phieuGoiTien)
+                {
+                    var formatNgayGoiTien = String.Format("{0:d}", itemPhieuGoiTien.NgayGoi);
+
+                    if (formatDateInput == formatNgayGoiTien)
+                    {
+                        sumGoi += (int)itemPhieuGoiTien.SoTienGoi;
+                    }
+                }
+            }
+            return sumGoi;
 
         }
+        public double TongTienRut1Ngay(string strLoaiTietKiem)
+        {
+            int sumRut = 0;
+            var inputSTK = ListSTK.Where(x => x.LOAITIETKIEM.TenLoaiTietKiem == strLoaiTietKiem);
+            var formatDateInput = String.Format("{0:d}", DateInput);
+            foreach (var item in inputSTK)
+            {
+                // var formatMoSo = String.Format("{0:d}", item.NgayMoSo);
+                // if (formatDateInput==formatMoSo)
+                //  {
+                var phieuRutTien = ListRutTien.Where(x => x.MaSo == item.MaSo);
+                foreach (var itemPhieuRutTien in phieuRutTien)
+                {
+                    var formatNgayRutTien = String.Format("{0:d}", itemPhieuRutTien.NgayRut);
+
+                    if (formatDateInput == formatNgayRutTien)
+                    {
+                        sumRut += (int)itemPhieuRutTien.SoTienRut;
+                    }
+                }
+            }
+            return sumRut;
+        }
+        public void TongSo(string strLoaiTietKiem, int soThang)
+        {
+            int sumTongSoMo = 0;
+            int sumTongSoDong = 0;
+            int stt = 0;
+            var listSTK2 = ListSTK.Where(x => x.LOAITIETKIEM.TenLoaiTietKiem == strLoaiTietKiem);
+            foreach(var item in listSTK2)
+            {
+
+                DateTime ngayMoSo = (DateTime)item.NgayMoSo;
+                //var formatngayTinhLaiGanNhat = String.Format("{0:d}", ngayTinhLaiGanNhat);
+                if (ngayMoSo.Month == soThang)
+                {
+                    stt++;
+                    List2.Add(new ThongKe2 { STT = stt, Ngay= ngayMoSo,SoMo = item.MaSo.ToString(), SoDong = "Null", ChenhLech2 = 0 });
+                }
+            }
+            foreach (var item in listSTK2)
+            {
+                DateTime ngayTinhLaiGanNhat = (DateTime)item.NgayTinhLaiGanNhat;
+                //var formatngayTinhLaiGanNhat = String.Format("{0:d}", ngayTinhLaiGanNhat);
+                if (ngayTinhLaiGanNhat.Month == soThang&&item.BiXoa==true)
+                {
+                    stt++;
+                    List2.Add(new ThongKe2 { STT = stt, Ngay = ngayTinhLaiGanNhat, SoMo = "Null", SoDong = item.MaSo.ToString(), ChenhLech2 = 0 });
+                }
+            }
+
+
+            /*  for (int i=0;i<ListSTK.Count();i++)
+              {
+                  DateTime ngayMoSo = (DateTime)ListSTK[i].NgayMoSo;
+                  var formatngayMoSo = String.Format("{0:d}", ngayMoSo);
+                  DateTime ngayTinhLaiGanNhat = (DateTime)ListSTK[i].NgayTinhLaiGanNhat;
+                  var formatngayTinhLaiGanNhat = String.Format("{0:d}", ngayTinhLaiGanNhat);
+                  if ((ngayMoSo.Month == soThang||(ngayTinhLaiGanNhat.Month==soThang&& ListSTK[i].BiXoa==true))&&ListSTK[i].LOAITIETKIEM.TenLoaiTietKiem==strLoaiTietKiem)
+                  {
+                      for(int j=i;j<ListSTK.Count();j++)
+                      {
+                          DateTime ngayMoSo2 = (DateTime)ListSTK[j].NgayMoSo;
+                          var formatngayMoSo2 = String.Format("{0:d}", ngayMoSo2);
+                          DateTime ngayTinhLaiGanNhat2 = (DateTime)ListSTK[j].NgayTinhLaiGanNhat;
+                          var formatngayTinhLaiGanNhat2 = String.Format("{0:d}", ngayTinhLaiGanNhat2);
+
+                          if (formatngayMoSo2==formatngayMoSo&&ListSTK[j].LOAITIETKIEM.TenLoaiTietKiem==strLoaiTietKiem)
+                          {
+                              MessageBox.Show("vo so mo" + "- thang:" + soThang);
+
+                              sumTongSoMo += 1;
+                          }
+                          if((formatngayTinhLaiGanNhat2==formatngayTinhLaiGanNhat)&& ListSTK[j].BiXoa==true && ListSTK[j].LOAITIETKIEM.TenLoaiTietKiem == strLoaiTietKiem)
+                          {
+                              MessageBox.Show("vo so dong" + "- thang:" + soThang);
+
+                              sumTongSoDong += 1;
+                          }
+
+                      }
+                      if (sumTongSoMo != 0||sumTongSoDong!=0)
+                      {
+                          i++;
+                          if(sumTongSoMo!=0 && sumTongSoDong==0)
+                              List2.Add(new ThongKe2 { STT = i,Ngay=ngayMoSo ,SoMo = sumTongSoMo, SoDong = sumTongSoDong, ChenhLech2 = 0 });
+                          else if(sumTongSoDong!=0 && sumTongSoMo == 0)
+                          {
+                              List2.Add(new ThongKe2 { STT = i,Ngay=ngayTinhLaiGanNhat, SoMo = sumTongSoMo, SoDong = sumTongSoDong, ChenhLech2 = 0 });
+                          }
+                          else
+                              List2.Add(new ThongKe2 { STT = i, Ngay=ngayMoSo,SoMo = sumTongSoMo, SoDong = sumTongSoDong, ChenhLech2 = 0 });
+                          sumTongSoDong = 0;
+                          sumTongSoMo = 0;
+
+                      }
+                  }
+              }*/
+        }
+        public int TongSoDong(string strLoaiTietKiem, int soThang)
+        {
+            int sumTongSo = 0;
+            var inputSTK = ListSTK.Where(x => x.BiXoa == true);
+            foreach (var item in inputSTK)
+            {
+                DateTime ngayTinhLaiGanNhat = (DateTime)item.NgayTinhLaiGanNhat;
+                if (ngayTinhLaiGanNhat.Month == soThang)
+                {
+                    sumTongSo += 1;
+                }
+            }
+            return sumTongSo;
+        }
+    }
+  
+  
+
+    public class ThongKe1
+    {
+        public int STT { get; set; }
+        public string LoaiTietKiem { get; set; }
+        public int TongChi { get; set; }
+        public int TongThu { get; set; }
+        public int ChenhLech { get; set; }
+
+    }
+    public class ThongKe2
+    {
+        public int STT { get; set; }
+        public DateTime Ngay { get; set; }
+        public string SoMo { get; set; }
+        public string SoDong { get; set; }
+        public int ChenhLech2 { get; set; }
+
     }
 }
--- QuanLySoTietKiem/QuanLySoTietKiem/ViewModel/WithdrawViewModel.cs
+++ QuanLySoTietKiem/QuanLySoTietKiem/ViewModel/WithdrawViewModel.cs
@@ -19,7 +19,7 @@ namespace QuanLySoTietKiem.ViewModel
         private string _MaSo;
         public string MaSo { get => _MaSo; set { _MaSo = value; CheckMaSTK(_MaSo); OnPropertyChanged(); } }
         private string _SoTienRut;
-        public string SoTienRut { get => _SoTienRut; set { _SoTienRut = value; OnPropertyChanged(); } }
+        public string SoTienRut { get => _SoTienRut; set { _SoTienRut = value; checkTienRut(MaSo); OnPropertyChanged(); } }
         private string _SoTienGui;
         public string SoTienGui { get => _SoTienGui; set { _SoTienGui = value; OnPropertyChanged(); } }
         private string _LoaiSTK;
@@ -27,6 +27,7 @@ namespace QuanLySoTietKiem.ViewModel
 
         private string _CMND;
         public string CMND { get => _CMND; set { _CMND = value; OnPropertyChanged(); } }
+        bool clicked = false;
         public ICommand AddCommand { get; set; }
         public ICommand ExitCommand { get; set; }
         public ICommand SaveCommand { get; set; }
@@ -40,15 +41,20 @@ namespace QuanLySoTietKiem.ViewModel
             });
             ExitCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
             {
+                ResetField();
                 p.Close();
             });
             SaveCommand = new RelayCommand<Window>(
                 (p) =>
                 {
-                    return !(String.IsNullOrEmpty(MaSo) );
+                    if ((String.IsNullOrEmpty(MaSo)) != true && clicked==true )
+                        return true;
+                    else
+                        return false;
                 },
                 (p) =>
                 {
+                    MessageBox.Show("vao chua dkm");
                     AddDeposit();
                     ResetField();
                 });
@@ -65,24 +71,83 @@ namespace QuanLySoTietKiem.ViewModel
         {
             bool isIntMaSo = int.TryParse(maSTK, out int mastk);
 
-            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == mastk && x.BiXoa == false).SingleOrDefault();
-            if (stk == null) ResetField();
+            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == mastk && x.BiXoa != true).SingleOrDefault();
+            if (stk == null)
+            {
+                clicked = false;
+                ResetField();
+            }
             else
             {
                 CMND = stk.KHACHHANG.CMND;
                 TenKhachHang = stk.KHACHHANG.TenKhachHang;
                 LoaiSTK = stk.LOAITIETKIEM.TenLoaiTietKiem.ToString();
-                SoTienGui = "Số dư: " + stk.SoTienGoi.ToString()+ " VNĐ";
+                SoTienGui = "Số dư khả dụng: " + TinhSoDu(maSTK) + " VNĐ";
                 TimeSpan timedate = (TimeSpan)(DateTime.Now - stk.NgayMoSo);
-                if(LoaiSTK=="Không kì hạn")
+                if (stk.LOAITIETKIEM.TenLoaiTietKiem == "Không kì hạn")
                 {
-                    if (kiemTraDateKhongKyHan((DateTime)stk.NgayMoSo))
-                        MessageBox.Show(DateTime.Now + "---" + stk.NgayMoSo + "= " + timedate.Days.ToString());
-                    else
+                    if ((DateTime.Now - (DateTime)stk.NgayMoSo).TotalDays < stk.LOAITIETKIEM.ThoiGianGoiToiThieu)
                         MessageBox.Show("Chưa đủ 15 ngày để rút tiền. Số ngày mới gửi được: " + timedate.Days + " ngày");
+
+                    else
+                        MessageBox.Show(DateTime.Now + "---" + stk.NgayMoSo + "= " + timedate.Days.ToString());
+                }
+                else
+                {
+                    SoTienRut = TinhSoDu(maSTK).ToString();
+
                 }
 
+
             }              
+        }
+        private bool checkTienRut(string maSTK)
+        {
+            bool isIntMaSo = int.TryParse(maSTK, out int mastk);
+            bool isIntSoTienRut = int.TryParse(SoTienRut, out int soTienRut);
+            if (soTienRut == 0)
+            {
+                clicked = false;
+                return false;
+            }
+            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == mastk && x.BiXoa != true).SingleOrDefault();
+            if (stk == null)
+            {
+                clicked = false;
+                return false;
+            }
+            else
+            {
+                if(stk.LOAITIETKIEM.TenLoaiTietKiem=="Không kì hạn")
+                {
+                    if (soTienRut <= TinhSoDu(stk.MaSo.ToString()))
+                        {
+                        clicked = true;
+                        return true;
+                    }
+                    else {
+                        clicked = false;
+                        return false; 
+                    }
+                }
+                else
+                {
+                    if (soTienRut!=TinhSoDu(mastk.ToString()))
+                    {
+                        SoTienRut = TinhSoDu(maSTK).ToString();
+
+                         clicked = true;
+                          return true;
+                    }
+                    else
+                    {
+                        clicked = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+
         }
         private bool kiemTraDateKhongKyHan(DateTime ngayMoSo)
         {
@@ -91,22 +156,81 @@ namespace QuanLySoTietKiem.ViewModel
             if (timedate.Days > 15) return true;
             return false;
         }
+        private double TinhSoDu(string maSTK)
+        {
+            long soDu = 0;
+            bool isIntMaSo = int.TryParse(maSTK, out int _mastk);
+            bool isIntSoTienRut = int.TryParse(SoTienRut, out int soTienGoi);
+            if (!isIntMaSo ) return 0;
+
+            var countPhieuRutTien = DataProvider.Ins.DB.PHIEURUTTIENs.Where(x => x.MaSo == _mastk && x.BiXoa !=true).Count();
+            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == _mastk && x.BiXoa != true).SingleOrDefault();
+            if (stk.LOAITIETKIEM.TenLoaiTietKiem == "Không kì hạn")
+            {
+                if ((DateTime.Now - (DateTime)stk.NgayMoSo).TotalDays < stk.LOAITIETKIEM.ThoiGianGoiToiThieu) return 0;
+                else
+                soDu= (long)(stk.SoTienGoi+((int)((decimal)stk.SoTienGoi * (decimal)stk.LOAITIETKIEM.LaiSuat * (decimal)(DateTime.Now - (DateTime)stk.NgayTinhLaiGanNhat).TotalDays / 36000)));
+              //  MessageBox.Show(((decimal)stk.SoTienGoi * (decimal)stk.LOAITIETKIEM.LaiSuat * (decimal)(DateTime.Now - (DateTime)stk.NgayTinhLaiGanNhat).TotalDays / 36000).ToString());
+                return soDu;
+            }
+
+            else
+            {
+                if ((DateTime.Now - (DateTime)stk.NgayMoSo).TotalDays < stk.LOAITIETKIEM.ThoiGianGoiToiThieu)
+                {
+                    MessageBox.Show("Chưa đến kì hạn tính lãi suất để có thể rút tiền!");
+                    clicked = false;
+                    return 0;
+                }
+                var koKiHan = DataProvider.Ins.DB.LOAITIETKIEMs.Where(x => x.TenLoaiTietKiem == "Không kì hạn").SingleOrDefault();
+                if (koKiHan == null) return 0;
+                var laiSuatKoKiHan = koKiHan.LaiSuat;
+                int laiKiHan;
+                int laiKhongKiHan;
+                if (((DateTime)stk.NgayTinhLaiGanNhat - (DateTime)stk.NgayMoSo).TotalDays < stk.LOAITIETKIEM.ThoiGianGoiToiThieu)
+                {
+                 //   MessageBox.Show("cc");
+                    laiKiHan = (int)((decimal)stk.SoTienGoi * (decimal)stk.LOAITIETKIEM.LaiSuat * stk.LOAITIETKIEM.ThoiGianGoiToiThieu / 36000);
+                    laiKhongKiHan = (int)((decimal)stk.SoTienGoi * (decimal)laiSuatKoKiHan * (decimal)((DateTime.Now - (DateTime)stk.NgayTinhLaiGanNhat).TotalDays - stk.LOAITIETKIEM.ThoiGianGoiToiThieu) / 36000);
+                }
+                else
+                {
+                    laiKiHan = 0;
+                    laiKhongKiHan = (int)((decimal)stk.SoTienGoi * (decimal)laiSuatKoKiHan * (decimal)(DateTime.Now - (DateTime)stk.NgayTinhLaiGanNhat).TotalDays / 36000);
+                }
+                soDu = (int)(stk.SoTienGoi+ laiKiHan + laiKhongKiHan);
+                return soDu;
+            }
+
+        }
         private void AddDeposit()
         {
 
             bool isIntMaSo = int.TryParse(MaSo, out int maSo);
             bool isIntSoTienRut = int.TryParse(SoTienRut, out int soTienRut);
             if (!isIntMaSo || !isIntSoTienRut) return;
-            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == maSo && x.BiXoa == false).SingleOrDefault();
+
+            var stk = DataProvider.Ins.DB.SOTIETKIEMs.Where(x => x.MaSo == maSo && x.BiXoa !=true).SingleOrDefault();
             if (stk == null) return;
+
             CheckMaSTK(MaSo);
-            if (stk.KHACHHANG.TenKhachHang == TenKhachHang && stk.KHACHHANG.CMND == CMND)
+            if (checkTienRut(MaSo))
             {
+                MessageBox.Show("Vô nút save"+ "sotiengoi: "+stk.SoTienGoi);
+                stk.SoTienGoi = (int)(TinhSoDu(MaSo) - soTienRut);
+                MessageBox.Show(stk.SoTienGoi.ToString()+"= sodu:"+TinhSoDu(MaSo)+"+ sotienrut: "+soTienRut);
+                stk.NgayTinhLaiGanNhat = DateTime.Now;
+                if (stk.SoTienGoi == 0)
+                {
+                    stk.BiXoa = true;
+                }
+                
                 var PHIEURUTTIEN = new PHIEURUTTIEN { SOTIETKIEM = stk, SoTienRut = soTienRut, MaSo = maSo, NgayRut = DateTime.Now };
                 DataProvider.Ins.DB.PHIEURUTTIENs.Add(PHIEURUTTIEN);
                 DataProvider.Ins.DB.SaveChanges();
-                //List.Add(PHIEURUTTIEN);
+                List.Add(PHIEURUTTIEN);
             }
+
         }
     }
 }
